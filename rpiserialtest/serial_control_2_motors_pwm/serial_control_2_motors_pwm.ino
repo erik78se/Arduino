0f65f57020c8dbd113efab286439bc642fb674ae
Erik Lönroth <erik.lonroth@gmail.com>
2016-05-15 23:52:29 +0200

    Code cleanup

--- rpiserialtest/serial_control_2_motors_pwm/serial_control_2_motors_pwm.ino
+++ rpiserialtest/serial_control_2_motors_pwm/serial_control_2_motors_pwm.ino
@@ -25,17 +25,23 @@ ros::NodeHandle nh;
 
 geometry_msgs::Twist twist_msg;
 
+// The PWM values we are going to use
 double lspeed = 0;
 double rspeed = 0;
 
 void MotorSpeeds(float left, float right) {
+
+  // Raise values -0.5 - +0.5 with a factor of 255
+  // So that we can have useful PWM values (0-255)
   lspeed = left*255;
   rspeed = right*255;
   
   analogWrite(E1, lspeed);  // Set left motor speed
   analogWrite(E2, rspeed);  // Set right motor speed
 
-  // Set direction of the two motors based on the sign of the speed
+
+  // Motor direction is determined based on the sign of the left,right values
+  // Set direction of the motors based on the sign of the speed
   if ( left >= 0 ) {
     digitalWrite(I1, HIGH);
     digitalWrite(I2, LOW);
@@ -57,35 +63,30 @@ void MotorSpeeds(float left, float right) {
 // Motor drive
 //
 // TODO: investigate https://github.com/joshvillbrandt/legorov-ros-pkg/blob/master/arduino/LegorovController/LegorovController.pde
-//
+// TODO: Make use of the angular rotation to be able to make the motors run at different speed and thus be able to turn
 // cmd_x  : linear x velocity (forward velocity) m/s
 // cmd_th : angular z velocity (rotation of heading) rad/s
 void diff_drive(float cmd_x, float cmd_th) {
-  float speed_wish_right = cmd_th*M_PI*WHEEL_DIST/2 + cmd_x;
-  float speed_wish_left = cmd_x*2-speed_wish_right;
-  /* This is where we acctually move motors later.
-   *  For now, just print to console.
-   */
+  // float speed_wish_right = cmd_th*M_PI*WHEEL_DIST/2 + cmd_x;
+  // float speed_wish_left = cmd_x*2-speed_wish_right;
+  
   char log_msg[200];
   // dtostrf(floatVar, minStringWidthIncDecimalPoint, numVarsAfterDecimal, charBuf);
   dtostrf(cmd_x*255, 1, 4, log_msg);
   nh.loginfo(log_msg);
-  
-  
-  // sprintf(log_msg, "R: %d", (double)(speed_wish_left));
-  // nh.loginfo(log_msg); 
 
+  // Set the speed of the motors accordingly
   MotorSpeeds(cmd_x,cmd_x);
-  
 }
 
+// The callback function
 void cmd_vel_callback( const geometry_msgs::Twist& twist_msg) {
   diff_drive(twist_msg.linear.x, twist_msg.angular.z);
 }
 
 ros::Subscriber<geometry_msgs::Twist> cmd_vel_topic("cmd_vel", &cmd_vel_callback);
 
-
+// Setup the pin numbers.
 void setupPins() {
   pinMode(E1, OUTPUT);
   pinMode(E2, OUTPUT);
@@ -106,8 +107,6 @@ void setup() {
   setupPins();
   nh.loginfo("Pins setup complete");
 }
-
-
 
 
 void loop() {
